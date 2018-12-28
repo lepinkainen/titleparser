c240dbc4b781ff24690a235b4843e51a564de1dd
Riku Lindblad <riku.lindblad@gmail.com>
2018-12-28 16:27:54 +0200

    Fix areena test after refactoring

--- handler/areena_test.go
+++ handler/areena_test.go
@@ -2,7 +2,7 @@ package handler
 
 import "testing"
 
-func TestHandleAreena(t *testing.T) {
+func TestYleAreena(t *testing.T) {
 	type args struct {
 		url string
 	}
@@ -16,13 +16,13 @@ func TestHandleAreena(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := HandleAreena(tt.args.url)
+			got, err := YleAreena(tt.args.url)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("HandleAreena() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("YleAreena() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if got != tt.want {
-				t.Errorf("HandleAreena() = %v, want %v", got, tt.want)
+				t.Errorf("YleAreena() = %v, want %v", got, tt.want)
 			}
 		})
 	}
