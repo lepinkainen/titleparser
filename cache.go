c66575f939a780090253768669284e60ac5c833f
Riku Lindblad <riku.lindblad@gmail.com>
2019-01-01 01:50:34 +0200

    More restructuring

---
+++ cache.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"errors"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	log "github.com/sirupsen/logrus"
+)
+
+// CheckCache will return a non-empty string if the URL given is in the cache
+func CheckCache(query TitleQuery) (string, error) {
+	// TODO:
+	// connect to dynamodb
+	// attempt to fetch url with query.URL
+	// return title
+	// optionally update ttl in DB
+	return "", errors.New("Cache miss")
+}
+
+// CacheAndReturn inserts a successfully found title to cache
+func CacheAndReturn(query TitleQuery, title string, err error) (TitleQuery, error) {
+	if err != nil {
+		query.Title = ""
+		return query, err
+	}
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1")},
+	)
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+	// create a map for DD
+	query.Title = title
+	query.Added = time.Now().Unix()
+	query.TTL = time.Now().Unix() + 86400 // 24 hours
+
+	log.Infof("Storing TitleQuery: %v", query)
+
+	av, err := dynamodbattribute.MarshalMap(query)
+	// construct an input that DD canhandle
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String("urls"),
+	}
+	// put item in DD
+	_, err = svc.PutItem(input)
+
+	return query, err
+}
---
+++ lambda/main.go
@@ -0,0 +1,80 @@
+package lambda
+
+import (
+	"context"
+	"os"
+	"regexp"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/lepinkainen/titleparser/handler"
+)
+
+var (
+	handlerFunctions = make(map[string]func(string) (string, error))
+)
+
+type TitleQuery struct {
+	Added   int64  `json:"timestamp"`
+	User    string `json:"user"`
+	Channel string `json:"channel"`
+	URL     string `json:"url"`
+	Title   string `json:"title"`
+	TTL     int64  `json:"ttl"` // TTL is used to expire the item in DynamoDB automatically
+}
+
+type handlerFunc func(string) (string, error)
+
+// RegisterParser adds the given url parser and pattern to the map of handlers
+func RegisterParser(pattern string, function handlerFunc) {
+	handlerFunctions[pattern] = function
+}
+
+// HandleRequest is the function entry point
+func HandleRequest(ctx context.Context, query TitleQuery) (TitleQuery, error) {
+
+	// open session to dynamoDB
+
+	log.Infof("Handling %v", query)
+
+	// if query is cached, return from cache instead of fetching
+	if title, err := handler.CheckCache(query); err == nil {
+		return handler.CacheAndReturn(query, title, nil)
+	}
+
+	// register custom parsers
+	RegisterParser(".*?areena.yle.fi/.*", handler.YleAreena)
+	RegisterParser(".*?apina.biz.*", handler.ApinaBiz)
+	//RegisterParser(".*", handler.DefaultHandler)
+
+	for pattern, handler := range handlerFunctions {
+		match, err := regexp.MatchString(pattern, query.URL)
+
+		// error in matching, log and continue
+		if err != nil {
+			log.Errorf("Error matching with pattern %s: %v", pattern, err)
+		}
+
+		// no error and match, run function to get actual title and return
+		if err == nil && match {
+			title, err := handler(query.URL)
+			return CacheAndReturn(query, title, err)
+		}
+	}
+
+	// custom parsers didn't match, use the default parser
+	title, err := handler.DefaultHandler(query.URL)
+	return CacheAndReturn(query, title, err)
+}
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.InfoLevel)
+}
