3a92123b5457f20263cad95d41c9e76356adfc47
Riku Lindblad <riku.lindblad@gmail.com>
2019-02-20 23:17:19 +0200

    Add IMDb handler

---
+++ handler/imdb.go
@@ -0,0 +1,92 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"regexp"
+
+	"github.com/lepinkainen/titleparser/lambda"
+	"github.com/pkg/errors"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	// url pattern for OMDB searches
+	omdbURL = "http://www.omdbapi.com/?i=%s&apikey=%s"
+	// figure out imdb id from url
+	imdbRe = regexp.MustCompile(`https://www.imdb.com/title/(tt.*?)/.*`)
+)
+
+type omdbReply struct {
+	Title   string `json:"Title"`
+	Year    string `json:"Year"`
+	Runtime string `json:"Runtime"`
+	Genre   string `json:"Genre"`
+	Ratings []struct {
+		Source string `json:"Source"`
+		Value  string `json:"Value"`
+	} `json:"Ratings"`
+	Metascore  string `json:"Metascore"`
+	ImdbRating string `json:"imdbRating"`
+	ImdbVotes  string `json:"imdbVotes"`
+}
+
+// OMDB handler
+func OMDB(url string) (string, error) {
+	omdbKey := os.Getenv("OMDB_KEY")
+	if omdbKey == "" {
+		return "", errors.New("No API key set for OMDB")
+	}
+
+	id := imdbRe.FindStringSubmatch(url)
+	if len(id) < 2 {
+		return "", errors.New("No title ID found in URL")
+	}
+
+	// Request the HTML page.
+	res, err := http.Get(fmt.Sprintf(omdbURL, id[1], omdbKey))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		return "", errors.Wrap(err, "HTTP error")
+	}
+
+	bytes, err := ioutil.ReadAll(res.Body)
+
+	var reply omdbReply
+
+	json.Unmarshal(bytes, &reply)
+
+	// Get possible scores from the Rating section
+	var rtScore = "N/A"
+	var imdbScore = "N/A"
+	var metaScore = "N/A"
+
+	for _, rating := range reply.Ratings {
+		switch rating.Source {
+		case "Rotten Tomatoes":
+			rtScore = rating.Value
+		case "Internet Movie Database":
+			imdbScore = rating.Value
+		case "Metacritic":
+			metaScore = rating.Value
+		}
+	}
+
+	// Ralph Breaks the Internet (2018)
+	title := fmt.Sprintf("%s (%s) [IMDb %s] [RT %s] [Meta %s]",
+		reply.Title, reply.Year, imdbScore, rtScore, metaScore)
+
+	return title, nil
+}
+
+func init() {
+	lambda.RegisterHandler("https://www.imdb.com/title/tt.*", OMDB)
+}
---
+++ handler/imdb_test.go
@@ -0,0 +1,37 @@
+package handler
+
+import "testing"
+
+// TODO: Make test use golden files instead of online testing
+func TestOMDB(t *testing.T) {
+	type args struct {
+		url string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{"The Matrix",
+			args{url: "https://www.imdb.com/title/tt0133093/"},
+			"The Matrix (1999) [IMDb 8.7/10] [RT 88%] [Meta 73/100]",
+			false},
+		{"No ID in URL",
+			args{url: "https://www.imdb.com/"},
+			"",
+			true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := OMDB(tt.args.url)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("OMDB() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("OMDB() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
