c0506245a6b06a1c5ea0fa980cb4a8ce48ddcc1b
Riku Lindblad <riku.lindblad@iki.fi>
2020-06-18 08:58:28 +0300

    Update default_test.go

--- lambda/default_test.go
+++ lambda/default_test.go
@@ -14,7 +14,7 @@ func TestDefaultHandler(t *testing.T) {
 		want    string
 		wantErr bool
 	}{
-		{"ValidURL2", args{url: "https://www.is.fi/taloussanomat/oma-raha/art-2000005935228.html"}, "Marko odottaa innolla talven sähkölaskuja – suosittu lämpöpumppu tuo jopa 1 500 euron säästön", false},
+		{"ValidURL2", args{url: "https://www.is.fi/taloussanomat/oma-raha/art-2000005935228.html"}, "Marko odottaa innolla talven sähkölaskuja – suosittu lämpöpumppu tuo jopa 1 500 euron säästön - Oma raha - Ilta-Sanomat", false},
 		{"ValidURL3", args{url: "https://yle.fi/uutiset/3-10507654"}, "Jätteiden mukana palaa miljoonien edestä arvokkaita metalleja – tutkijat löysivät menetelmän, jolla ne voidaan saada talteen tuhkasta", false},
 		{"No opengraph", args{url: "https://www.manttavilppula.fi"}, "Taidekaupunki | Mänttä-Vilppula - Mänttä-Vilppula", false},
 		{"No title", args{url: "http://addiktit.net"}, "", true},
