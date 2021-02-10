56b84ce447f0e0d127a64bdab564dcec3bb6eadb
Riku Lindblad <riku.lindblad@iki.fi>
2021-02-10 10:55:46 +0000

    Fix broken test cases

--- handler/imgur_test.go
+++ handler/imgur_test.go
@@ -14,8 +14,8 @@ func TestImgur(t *testing.T) {
 		want    string
 		wantErr bool
 	}{
-		{"gallery test", args{url: "https://imgur.com/gallery/md2Sxjm"}, "Chill in the face of aggression. [tags: america, police brutality, protest, current events, take that]", false},
-		{"gallery test 2", args{url: "https://imgur.com/gallery/QXR4OL2"}, "Another Soulsborne dump? Why not! [44 images] [tags: Dark Souls, bloodborne, darksouls, dark souls 3, dark souls 2 is actually ok]", false},
+		{"gallery test", args{url: "https://imgur.com/gallery/md2Sxjm"}, "Chill in the face of aggression. [tags: protest, take that, current events, police brutality, america]", false},
+		{"gallery test 2", args{url: "https://imgur.com/gallery/QXR4OL2"}, "Another Soulsborne dump? Why not! [44 images] [tags: darksouls, bloodborne, Dark Souls, dark souls 3, dark souls 2 is actually ok]", false},
 		{"gallery test 3", args{url: "https://imgur.com/gallery/YCNzrKx"}, "As pertinent and poignant as ever. [tags: reaction, current events]", false},
 		{"album test", args{url: "https://imgur.com/a/X2PcObK"}, "Assembly 1998 prizegivign. [2 images]", false},
 		{"album test/no title", args{url: "https://imgur.com/a/MZY7mkE"}, "", false},
@@ -25,8 +25,8 @@ func TestImgur(t *testing.T) {
 		{"subreddit image test 2", args{url: "https://i.imgur.com/dJJbwhM.jpg"}, "Stay safe out there. [/r/MTB]", false},
 		{"subreddit image test direct 2", args{url: "https://imgur.com/dJJbwhM"}, "Stay safe out there. [/r/MTB]", false},
 		{"subreddit gifv test", args{url: "https://i.imgur.com/OiocRjL.gifv"}, "The correct usage of a phone [/r/gifs]", false},
-		{"tag gallery image", args{url: "https://imgur.com/t/funny/MWvY6dD"}, "Isolation day:....lost count [tags: funny, mildly interesting, the more you know, fart, armpit]", false},
-		{"tag gallery image 2", args{url: "https://imgur.com/t/fart/srK588K"}, "Sleepy Rue [tags: fart, sweet jesus, happy friday, doggo, daggs]", false},
+		{"tag gallery image", args{url: "https://imgur.com/t/funny/MWvY6dD"}, "Isolation day:....lost count [tags: armpit, mildly interesting, the more you know, funny, fart]", false},
+		{"tag gallery image 2", args{url: "https://imgur.com/t/fart/srK588K"}, "Sleepy Rue [tags: doggo, daggs, happy friday, sweet jesus, fart]", false},
 		{"Wrong URL", args{url: "http://mantta.fi"}, "", false},
 	}
 	for _, tt := range tests {
