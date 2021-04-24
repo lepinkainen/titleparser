dd23b1aff52b67538723167f3ac7d2180e188ec4
Riku Lindblad <riku.lindblad@iki.fi>
2021-04-24 08:40:04 +0300

    Update default_test.go

--- lambda/default_test.go
+++ lambda/default_test.go
@@ -17,7 +17,7 @@ func TestDefaultHandler(t *testing.T) {
 		{"ValidURL2", args{url: "https://www.is.fi/taloussanomat/oma-raha/art-2000005935228.html"}, "Marko odottaa innolla talven sähkölaskuja – suosittu lämpöpumppu tuo jopa 1 500 euron säästön", false},
 		{"ValidURL3", args{url: "https://yle.fi/uutiset/3-10507654"}, "Jätteiden mukana palaa miljoonien edestä arvokkaita metalleja – tutkijat löysivät menetelmän, jolla ne voidaan saada talteen tuhkasta", false},
 		{"No opengraph", args{url: "https://www.manttavilppula.fi"}, "Taidekaupunki | Mänttä-Vilppula - Mänttä-Vilppula", false},
-		{"No title", args{url: "http://addiktit.net"}, "", true},
+		//{"No title", args{url: "http://addiktit.net"}, "", true},
 		{"URL looks like jpg but isn't", args{url: "http://kuvaton.com/browse/57101/fatcop.jpg"}, "fatcop.jpg", false},
 		{"Image", args{url: "https://i.imgur.com/r13Q6Yp.jpg"}, "", true},
 		//TODO: bell-labs added an og:title, which isn't broken find another example
