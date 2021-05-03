a7bb350e8bc523ccd5f166fb8f335b4e47f31bb3
Riku Lindblad <riku.lindblad@iki.fi>
2021-05-03 22:57:21 +0300

    Fix better

--- handler/twitter_test.go
+++ handler/twitter_test.go
@@ -20,7 +20,7 @@ func TestTwitter(t *testing.T) {
 	}{
 		{"Kohli 1 - emoji,link", args{url: "https://twitter.com/RahulKohli13/status/1263946250077929473"}, `Rahul Kohli \(✔@RahulKohli13\) \d+(d|m|y): Becoming an actor can be incredibly challenging and extremely daunting but if you really want get off to a great start, stop using the fucking hashtag #ActorsLife on Instagram with your stupid fucking pictures of you posing at Starbucks, “prepping for your audition 💅🏽”. https://t.co/yeTkJkBx2Z \[. \d+ . \d+\]`, false},
 		{"Foone 1 - image", args{url: "https://twitter.com/Foone/status/1389076911138115587"}, `foone \(@Foone\) \d+(m|h|d|y): and then I flashed it back. This thing is magic. https://t.co/4n8UxvEhvG \[. \d+ . \d+\]`, false},
-		{"Foone 2 - video", args{url: "https://twitter.com/Foone/status/1250937428518436864"}, `foone \(@Foone\) \d+(m|h|d|y): So the show Shuriken Sentai Ninninger, the 39th show in the Super Sentai series (which formed the basis for the Power Rangers shows), had a monster named Yokai Mokumokuren, who is a supernaturally-infected computer keyboard. https://t.co/224u1P9iqb`, false},
+		{"Foone 2 - video", args{url: "https://twitter.com/Foone/status/1250937428518436864"}, `foone \(@Foone\) \d+(m|h|d|y): So the show Shuriken Sentai Ninninger, the 39th show in the Super Sentai series \(which formed the basis for the Power Rangers shows\), had a monster named Yokai Mokumokuren, who is a supernaturally-infected computer keyboard. https://t.co/224u1P9iqb \[. \d+ . \d+\]`, false},
 	}
 	for _, tt := range tests {
 		tt := tt
