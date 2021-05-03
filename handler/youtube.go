58ef84e479faecbc8d8c5f5c7a4e30be28d08dd0
Riku Lindblad <riku.lindblad@iki.fi>
2021-05-04 00:32:21 +0300

    Youtube handler
    Fixes #25

---
+++ handler/youtube.go
@@ -0,0 +1,149 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/dustin/go-humanize"
+	"github.com/lepinkainen/titleparser/lambda"
+	"github.com/pkg/errors"
+
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	apiURL = "https://www.googleapis.com/youtube/v3/videos"
+
+	youtubeRegex1 = regexp.MustCompile(`https?://youtu.be/([^\?]+)([\?#]t=.*)?`)
+	youtubeRegex2 = regexp.MustCompile(`https?://.*?youtube.com/watch\?.*?v=([^&#]+)`)
+)
+
+type YoutubeReply struct {
+	Etag  string `json:"etag"`
+	Items []struct {
+		ContentDetails struct {
+			Caption       string `json:"caption"`
+			ContentRating struct {
+				YtRating string `json:"ytRating"`
+			} `json:"contentRating"`
+			Definition      string `json:"definition"`
+			Dimension       string `json:"dimension"`
+			Duration        string `json:"duration"`
+			LicensedContent bool   `json:"licensedContent"`
+			Projection      string `json:"projection"`
+		} `json:"contentDetails"`
+		Etag    string `json:"etag"`
+		ID      string `json:"id"`
+		Kind    string `json:"kind"`
+		Snippet struct {
+			CategoryID           string `json:"categoryId"`
+			ChannelID            string `json:"channelId"`
+			ChannelTitle         string `json:"channelTitle"`
+			DefaultAudioLanguage string `json:"defaultAudioLanguage"`
+			Description          string `json:"description"`
+			LiveBroadcastContent string `json:"liveBroadcastContent"`
+			Localized            struct {
+				Description string `json:"description"`
+				Title       string `json:"title"`
+			} `json:"localized"`
+			PublishedAt string   `json:"publishedAt"`
+			Tags        []string `json:"tags"`
+			Title       string   `json:"title"`
+		} `json:"snippet"`
+		Statistics struct {
+			CommentCount  string `json:"commentCount"`
+			DislikeCount  string `json:"dislikeCount"`
+			FavoriteCount string `json:"favoriteCount"`
+			LikeCount     string `json:"likeCount"`
+			ViewCount     string `json:"viewCount"`
+		} `json:"statistics"`
+	} `json:"items"`
+}
+
+// OMDB handler
+func Youtube(url string) (string, error) {
+	//youtubeKey := os.Getenv("YOUTUBE_KEY")
+	youtubeKey := "AIzaSyD5a4Johhq5K0ARWX-rQMwsNz0vTtQbKNY"
+	if youtubeKey == "" {
+		return "", errors.New("No API key set for Youtube")
+	}
+
+	match := youtubeRegex1.FindStringSubmatch(url)
+	// Try again with second regex
+	if len(match) == 0 {
+		match = youtubeRegex2.FindStringSubmatch(url)
+		if len(match) == 0 {
+			return "", errors.New("Not a youtube URL")
+		}
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", apiURL, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	q := req.URL.Query()
+	q.Add("id", match[1])
+	q.Add("key", youtubeKey)
+	q.Add("part", "snippet,contentDetails,statistics")
+	q.Add("fields", "items(id,snippet,contentDetails,statistics)")
+	req.URL.RawQuery = q.Encode()
+
+	// Query the API
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		return "", errors.Wrap(err, "HTTP error")
+	}
+
+	// error ignored on purpose
+	bytes, _ := ioutil.ReadAll(res.Body)
+
+	// unmarshal JSON
+	var reply YoutubeReply
+
+	err = json.Unmarshal(bytes, &reply)
+	if err != nil {
+		return "", err
+	}
+
+	video := reply.Items[0]
+
+	// The tag value is an ISO 8601 duration in the format PT#M#S
+	duration := strings.ToLower(video.ContentDetails.Duration[2:])
+
+	ageRestricted := ""
+	if video.ContentDetails.ContentRating.YtRating == "ytAgeRestricted" {
+		ageRestricted = " - age restricted"
+	}
+
+	publishedAt, err := time.Parse(time.RFC3339, video.Snippet.PublishedAt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	agestr := humanize.RelTime(publishedAt, time.Now(), "ago", "from now")
+
+	viewCount, _ := strconv.ParseInt(video.Statistics.ViewCount, 10, 64)
+	views := HumanizeNumber(int(viewCount))
+
+	// Ralph Breaks the Internet (2018)
+	title := fmt.Sprintf("%s by %s [%s - %s views - %s%s]", video.Snippet.Title, video.Snippet.ChannelTitle, duration, views, agestr, ageRestricted)
+
+	return title, nil
+}
+
+func init() {
+	lambda.RegisterHandler("https://youtu.be/.*", Youtube)
+	lambda.RegisterHandler("https://youtube.com/watch.*", Youtube)
+}
---
+++ handler/youtube_test.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestYoutube(t *testing.T) {
+	type args struct {
+		url string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{"Marvel 1", args{url: "https://www.youtube.com/watch?v=QdpxoFcdORI"}, `Marvel Studios Celebrates The Movies by Marvel Entertainment \[3m11s - \d+M views - \d+ (hours|days|weeks) ago\]`, false},
+		{"Marvel 1 - short", args{url: "https://youtu.be/QdpxoFcdORI"}, `Marvel Studios Celebrates The Movies by Marvel Entertainment \[3m11s - \d+M views - \d+ (hours|days|weeks) ago\]`, false},
+		{"Age restricted", args{url: "https://www.youtube.com/watch?v=EX_8ZjT2sO4"}, `Grenouer - Alone in the Dark - \[UNCENSORED - AGE RESTRICTED\] by GrenouerVEVO \[3m59s - \d+M views - \d years ago - age restricted\]`, false},
+		{"At timestamp", args{url: "https://youtu.be/EX_8ZjT2sO4?t=98"}, `Grenouer - Alone in the Dark - \[UNCENSORED - AGE RESTRICTED\] by GrenouerVEVO \[3m59s - \d+M views - \d years ago - age restricted\]`, false},
+		{"Gangnam style", args{url: "https://www.youtube.com/watch?v=9bZkp7q19f0"}, `PSY - GANGNAM STYLE\(강남스타일\) M/V by officialpsy \[4m13s - \d+Billion views - \d+ years ago\]`, false},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got, err := Youtube(tt.args.url)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Youtube() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			match, err := regexp.MatchString(tt.want, got)
+			if err != nil || !match {
+				t.Errorf("Youtube() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
